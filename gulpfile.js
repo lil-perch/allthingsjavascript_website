331c118add90158f87522bd2b961577a0c661ac4
Steven Hancock <steveh@rapidintake.com>
2016-07-07 15:49:32 -0600

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+// Styles
+gulp.task('styles', function() {
+    console.log('starting styles task');
+});
+
+// Scripts
+gulp.task('scripts', function() {
+    console.log('starting scripts task');
+});
+// Images
+
+gulp.task('default', function() {
+    console.log('Starting default task');
+});
