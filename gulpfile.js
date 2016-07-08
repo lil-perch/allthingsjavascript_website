2d31cf972eadb28f1c6531e831102656ef9627f9
Steven Hancock <steveh@rapidintake.com>
2016-07-08 15:32:34 -0600

    Added watch and script task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
+//File paths
+var SCRIPTS_PATH = 'builds/public/assets/js/**/*.js';
 // Styles
 gulp.task('styles', function() {
     console.log('starting styles task');
@@ -9,9 +11,18 @@ gulp.task('styles', function() {
 // Scripts
 gulp.task('scripts', function() {
     console.log('starting scripts task');
+    return gulp.src(SCRIPTS_PATH)
+        .pipe(uglify())
+        .pipe(gulp.dest('builds/public/dist/js'));
 });
 // Images
 
 gulp.task('default', function() {
     console.log('Starting default task');
+});
+
+gulp.task('watch', function() {
+    console.log('Starting watch task');
+    require('./server.js');
+    gulp.watch(SCRIPTS_PATH, ['scripts']);
 });
